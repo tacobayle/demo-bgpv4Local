7ca2df6091158ad6905540713782b03ae7141f4b
Nicolas Bayle <nicolas.bayle.it@gmail.com>
2019-02-12 18:00:16 +0000

    20190212: tested

--- bgpv4Local.sh
+++ bgpv4Local.sh
@@ -3,12 +3,14 @@ echo "Copying the github repos"
 ansible-playbook git.yml
 echo "#####################################"
 echo "Copying the Ansible hostfile"
+cp hostsbgpv4Local aviLsc/hosts
 cp hostsbgpv4Local cs/hosts
 cp hostsbgpv4Local aviBootstrap/hosts
 cp hostsbgpv4Local aviLscCloud/hosts
 cp hostsbgpv4Local vyos/hosts
 echo "#####################################"
 echo "Copying the config files"
+cp params_aviLsc.yml aviLsc/vars/params.yml
 cp params_aviBootstrap.yml aviBootstrap/vars/params.yml
 cp vyos_bgpv4Local.conf vyos/config.conf
 cp params_vyos.yml vyos/vars/params.yml
@@ -16,7 +18,9 @@ cp params_cs cs/vars/params.yml
 cp params_aviLscCloud.yml aviLscCloud/vars/params.yml
 echo "#####################################"
 echo "Apply the configuration"
-cd vyos
+cd aviLsc
+ansible-playbook -i hosts generateCreds.yml
+cd ../vyos
 ansible-playbook -i hosts vyos.yml
 cd ../cs
 ansible-playbook -i hosts main.yml
